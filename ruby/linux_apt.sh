25970f9b6d8b07bc0d7ab0f85a6bfc5d06bf6e7f
Abhishek Modi <abhishek.modi95@gmail.com>
2015-04-10 11:30:34 -0500

    setting up file structure. Testing with Ruby

---
+++ ruby/linux_apt.sh
@@ -0,0 +1,28 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install git-core
+sudo apt-get install ruby
+sudo apt-get install ruby-dev
+sudo apt-get install libxslt1-dev
+sudo apt-get install libxml2-dev
+sudo apt-get install zlib1g-dev
+sudo apt-get install sqlite3
+sudo apt-get install libsqlite3-dev
+sudo apt-get install libssl-dev
+sudo apt-get install libyaml-dev
+sudo apt-get install libffi-dev
+sudo apt-get install build-essential
+
+sudo gem install rvm
+sudo gem install bundler
+sudo gem install nokogiri
+sudo gem install sqlite3
+sudo gem install rails
+sudo gem install railties
+sudo gem install executable-hooks
+sudo gem install gem-wrappers
+sudo gem install rubygems-bundler
+
+sudo apt-get install subversion
+sudo apt-get install git

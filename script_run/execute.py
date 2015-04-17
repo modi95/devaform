2ceace8ea398421e1a3e60c03f8e102a6bcd9521
Abhishek Modi <abhishek.modi95@gmail.com>
2015-04-17 18:04:49 -0500

    more stuff

--- script_run/execute.py
+++ script_run/execute.py
@@ -1,26 +1,50 @@
 import urllib
+import sys
 import os
 
 print ("/////////////////// DEVAFORM //////////////////")
 print ("// Terraforming Your Developer Environment ///")
 
 smooth = 0
-script = urllib.urlopen("http://www.akmodi.com/devaform/scripts/test.sh")
+#script = urllib.urlopen("http://www.akmodi.com/devaform/scripts/test.sh")
+#script = open("testing", "r")
+script = urllib.urlopen(sys.argv[1])
 last_command = ""
 last_number = 0
+last_arg = ""
 
 for line in script:
 	last_number += 1
-	last_command = line
-	smooth = os.system (line)
-	if smooth != 0:
+	last_command = line[0:line.index("\n")]
+	
+	if ("##_" in line):
+		last_arg = raw_input(last_command)
+
+	elif ("###" in line):
+		print line
+	
+	elif (last_arg is ""):
+		smooth = os.system (last_command + " > ong.dvt")
+	
+	else:
+		cmd = last_command
+		cmd += " " + last_arg[1:] + " > ong.dvt"
+		smooth = os.system (cmd)
+		last_arg = ""
+
+	if (smooth != 0 and last_arg is ""):
 		break
 
 if smooth != 0:
 	print ("Script failed at:")
 	print ("  line " + str(last_number))
 	print ("  ->" + last_command)
+	os.system ("rm ong.dvt")
+	exit()
 
-print ("/////////////////// DEVAFORM //////////////////")
+
+
+os.system ("rm ong.dvt")
+print ("\n/////////////////// DEVAFORM //////////////////")
 print ("////// Required Terraforming Completed ////////")
-print ("\n You should be all set-up now. But consider rebooting for good measure")
+print ("\nYou should be all set-up now.\nPlease this instance of terminal for good measure")

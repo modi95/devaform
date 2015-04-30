66c42b87902a94e77a37a581f28863032d491f28
Abhishek Modi <abhishek.modi95@gmail.com>
2015-04-30 04:30:58 -0500

    added stuff to flask config

--- python/flask/linux_apt.sh
+++ python/flask/linux_apt.sh
@@ -2,6 +2,23 @@
 
 
 sudo apt-get install python-virtualenv
+
 sudo pip install Flask
+sudo pip install Jinja2
+sudo pip install MarkupSafe
+sudo pip install MySQL-python
+sudo pip install Pafy
+sudo pip install SQLAlchemy
+sudo pip install Werkzeug
+sudo pip install argparse
+sudo pip install gevent
+sudo pip install greenlet
+sudo pip install gunicorn
+sudo pip install isodate
+sudo pip install itsdangerous
+sudo pip install mutagen
+sudo pip install requests
+sudo pip install wsgiref
+sudo pip install alembic
 
 echo "DEVAFORM///////// Completed installing Flask"
